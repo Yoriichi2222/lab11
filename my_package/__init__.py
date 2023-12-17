3dcd22a89522c0021d54c963114a0d500175c1d1
Yoriichi2222 <vs973222@gmail.com>
2023-12-17 22:00:53 +0500

    11
    11

--- my_package/__init__.py
+++
@@ -1,3 +0,0 @@
-from lab7 import count, calculate_xi
-from laba8 import make_calc, f
-from lab9 import generate_random_number
--- my_package/app.py
+++
@@ -1,6 +0,0 @@
-from my_package import count
-
-result = count([1, 2, 3])
-print(result)
-
-
